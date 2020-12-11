759533674f7e1177ce697b5106288c2607a137e3
Larry Volz <imaginologist@gmail.com>
2020-12-11 14:05:31 -0500

    win routines not working

--- connect-four-oo/connect4.js
+++ connect-four-oo/connect4.js
@@ -26,28 +26,29 @@ the four in a row before the win sequence
 
 
 class Game {
-  constructor (p1, p2, HEIGHT=6, WIDTH=7 ) {
-    this.players = [p1,p2];
+  constructor(p1, p2, HEIGHT = 6, WIDTH = 7) {
+    this.players = [p1, p2];
     this.WIDTH = WIDTH;
     this.HEIGHT = HEIGHT;
 
     this.PORTFOLIO = "https://larry-volz.github.io/Portfolio-for-Larry-Volz/";
-    this.COLOR = ["","red", "blue"];
+
+    this.COLOR = ["", "red", "blue"]; //???TODO
 
 
     this.currPlayer = p1; // active player: 1 or 2
-    
 
-    let winningFour = [[]];
 
-    this.makeBoard(); //??? Why???
-    this.makeHtmlBoard(); //??? Why???
+    this.winningFourXYs = [[]];
+
+    this.makeBoard(); 
+    this.makeHtmlBoard(); 
     // this.placeInTable();
     this.gameOver = false;
 
 
   }
-    
+
   // makeBoard();  //*********************** WHY THROWS ERROR?  CAN'T CALL FUNCTIONS WITHIN CLASS??? */
   // makeHtmlBoard;
 
@@ -55,34 +56,39 @@ class Game {
   //  *    board = array of rows, each row is array of cells  (board[y][x])
   //  */
   makeBoard() {
-  this.board = []; // // array of rows, each row is array of cells  (board[y][x])
-  for (let y = 0; y < this.HEIGHT; y++) {
-    this.board.push(Array.from({ length: this.WIDTH }));
+    this.board = []; // // array of rows, each row is array of cells  (board[y][x])
+    for (let y = 0; y < this.HEIGHT; y++) {
+      this.board.push(Array.from({ length: this.WIDTH }));
+    }
   }
-}
 
   /** makeHtmlBoard: make HTML table and row of column this.tops. */
   makeHtmlBoard() {
     const htmlBoard = document.querySelector("#board");
-  
+
     // TODO: Create this.top row & listener
     const top = document.createElement("tr");
     top.setAttribute("id", "column-top");
 
+
+
+    //************** IMPORTANT TO REVIEW ***************** */
     //HAVE TO BIND HERE SINCE IT IS GOING IN AN INSIDE METHOD
     this.handleGameClick = this.handleClick.bind(this);
-  
-    top.addEventListener("click", this.handleGameClick);    //******************ERROR HERE ********** */
-  
+    //************** IMPORTANT TO REVIEW ***************** */
+
+
+    top.addEventListener("click", this.handleGameClick);    
+
     for (let x = 0; x < this.WIDTH; x++) {
       const headCell = document.createElement("td");
       headCell.setAttribute("id", x);
       top.append(headCell);
     }
     htmlBoard.append(top);
-  
+
     // Creates grid
-    for ( let y = 0; y < this.HEIGHT; y++) {
+    for (let y = 0; y < this.HEIGHT; y++) {
       const row = document.createElement("tr");
       for (let x = 0; x < this.WIDTH; x++) {
         const cell = document.createElement("td");
@@ -93,162 +99,165 @@ class Game {
     }
   }
 
-//** findSpotForCol: given column x, return this.top empty y (null if filled) */
-findSpotForCol(x) { 
-  // finds the empty vertical slot for a given x value
-  for (let y = this.HEIGHT-1; y >= 0; y--) {
-    if (!this.board[y][x]) {
-      return y;
+  //** findSpotForCol: given column x, return this.top empty y (null if filled) */
+  findSpotForCol(x) {
+    // finds the empty vertical slot for a given x value
+    for (let y = this.HEIGHT - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
+      }
     }
+    return null;
   }
-  return null;
-}
 
 
-/** placeInTable: update DOM to place piece into HTML table of board */
+  /** placeInTable: update DOM to place piece into HTML table of board */
 
   placeInTable(y, x) {
-  // Makes a div and inserts into correct table cell
-  const div = document.createElement("div");
-  div.classList.add("piece");
-  div.classList.add(`p${this.currPlayer}`);
-  //----------------------------------------------- DO COLOR HERE! -------------
-  // div.style.backgroundColor = COLOR[currPlayer-1];
-  const cell = document.getElementById(`${y}-${x}`);
-  console.log("cell",cell);
-  cell.append(div);
-  console.log(`p${this.currPlayer} played ${y}-${x}`);
-
-}
-
-//functions to check for 4 in a row
-
-getHoriz(y, x) { 
-  [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-}
-getVert(y,x) { 
-  [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-}
-getDiagDR (y,x) {
-  [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-}
-getDiagDL(y,x) {
-  [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-}
-
-
-/** endGame: announce game end */
-
-endGame(msg) {
-
-  //visually highlights the disks that are 4 in a row 
-  for ( let disk = 0; disk < 4; disk++) {
-    y1 = winningFour[disk][0];
-    x1 = winningFour[disk][1]
-    let highlight = document.getElementById(`${y1}-${x1}`);
-    highlight.classList.add(`p${this.currPlayer}Win`);
+    // Makes a div and inserts into correct table cell
+    const div = document.createElement("div");
+    div.classList.add("piece");
+    div.classList.add(`p${this.currPlayer}`);
+    //----------------------------------------------- DO COLOR HERE! -------------
+    // div.style.backgroundColor = COLOR[currPlayer-1];
+    const cell = document.getElementById(`${y}-${x}`);
+    // console.log("cell", cell);
+    cell.append(div); //places piece in the form of a div inside a td
+    console.log(`p${this.currPlayer} played ${y}-${x}`);
 
   }
-  // Pops up winning alert message
-  //used setTimeout because the alert was popping up before the screen had the chance
-  //to re-draw the piece which was VERY unsatisfying to the players
-  
-  setTimeout(() => {
-    let playAgain = confirm(msg);
-    if(playAgain) {location.reload()}
-    else (window.location.replace(PORTFOLIO));
-    
-  }, 2);
-}
 
-
-// // /* ---------------------------------- MAIN GAME LOOP from EventListener -----------------------------------------*/
-
-/** handleClick: handle click of column this.top to play piece */
- handleClick(evt) { //---------------------------- ERROR SAYS THIS IS NOT DEFINED - A THIS THING ------
-  // get x from ID of clicked cell
-  const x = +evt.target.id;
-
-  // get next spot in column (if none, ignore click)
-  const y = this.findSpotForCol(x);
-  if (y === null) {
-    return;
+  //functions to check for 4 in a row
+  //returns an array of WHICH y,x coordinates to look at
+  getHoriz(y, x) {
+    return [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+  }
+  getVert(y, x) {
+    return [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+  }
+  getDiagDR(y, x) {
+    return [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+  }
+  getDiagDL(y, x) {
+    return [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
   }
 
-  // place piece in board and add to HTML table
-  this.placeInTable(y, x);
 
-  // TODO: add line to update in-memory board
-  this.board[y][x] = this.currPlayer;
+  /** endGame: announce game end */
 
-  // check for win
+  endGame(msg) {
+    console.log("MADE IT TO ENDGAME()")
+    //visually highlights the disks that are 4 in a row 
+    for (let disk = 0; disk < 4; disk++) {
+      y1 = winningFourXYs[disk][0];
+      x1 = winningFourXYs[disk][1]
+      let highlight = document.getElementById(`${y1}-${x1}`);
+      highlight.classList.add(`p${this.currPlayer}Win`);
 
-  if (this.checkForWin()) {
-    return endGame(`${this.COLOR[this.currPlayer].toUpperCase()} Wins!\nWant to play again?`);
+    }
+    // Pops up winning alert message
+    //used setTimeout because the alert was popping up before the screen had the chance
+    //to re-draw the piece which was VERY unsatisfying to the players
+
+    setTimeout(() => {
+      let playAgain = confirm(msg);
+      if (playAgain) { location.reload() }
+      else (window.location.replace(PORTFOLIO));
+
+    }, 2);
   }
 
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame ******************************
+
+  // // /* ---------------------------------- MAIN GAME LOOP from EventListener -----------------------------------------*/
+
+  /** handleClick: handle click of column this.top to play piece */
+  handleClick(evt) { // ********  <-- referred to as handleGameClick now via a bind() ********
+    // get x from ID of clicked cell
+    const x = +evt.target.id;
+
+    // get next spot in column (if none, ignore click)
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+
+    // place piece in board and add to HTML table
+    this.placeInTable(y, x);
+
+    // TODO: add line to update in-memory board
+    this.board[y][x] = this.currPlayer;
+
+    // check for win
+
+    if (this.checkForWin()) {
+      return endGame(`${this.COLOR[this.currPlayer].toUpperCase()} Wins!\nWant to play again?`);
+    }
+
+    // check for tie
+    // TODO: check if all cells in board are filled; if so call, call endGame ******************************
 
 
-  // switch players
-  // currPlayer 1 <-> 2
-  if (this.currPlayer === 1) { this.currPlayer = 2} else { this.currPlayer = 1};
-}
+    // switch players
+    // currPlayer 1 <-> 2
+    if (this.currPlayer === 1) { this.currPlayer = 2 } else { this.currPlayer = 1 };
+  }
 
-// /** checkForWin: check board cell-by-cell for "does a win start here?" */
+  // /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
-checkForWin() {
+  checkForWin() {
     // Check four cells to see if they're all color of current player
     // cells: list of four (y, x) cells
 
     //  returns true IF ALL are legal coordinates...
-
-    const _win = cells =>
-    {
-      cells.every( 
+    
+    const _win = cells => {
+      
+      cells.every(
         ([y, x]) =>
-        y >= 0 &&
-        y < this.HEIGHT &&
-        x >= 0 &&
-        x < this.WIDTH &&
-        // AND all match currPlayer (all the same color)
-        this.board[y][x] === this.currPlayer
-    );
-      }
+          y >= 0 &&
+          y < this.HEIGHT &&
+          x >= 0 &&
+          x < this.WIDTH &&
+          // AND all match currPlayer (all the same color)
+          this.board[y][x] === this.currPlayer
+      );
+    }
 
-  //Create all the sequences of 4 on the board and make into arrays of coordinates
-  for (let y = 0; y < this.HEIGHT; y++) {
-    for (let x = 0; x < this.WIDTH; x++) {
-      //for each column (x) check and see if there are 4 in a row horizontally
-      //make each check into a 2d array
-      let horiz = this.getHoriz(y, x);
-      //then vertically
-      let vert = this.getVert(y, x);
-      
-      //then for each diagonal direction
-      let diagDR = this.getDiagDR(y, x);
-      let diagDL = this.getDiagDL(y, x);
-      
+    //Create all the sequences of 4 on the board and make into arrays of coordinates
+    for (let y = 0; y < this.HEIGHT; y++) {
+      for (let x = 0; x < this.WIDTH; x++) {
+        //for each column (x) check and see if there are 4 in a row horizontally
+        //make each check into a 2d array
+        let horiz = this.getHoriz(y, x);
+        //then vertically
+        let vert = this.getVert(y, x);
+        //then for each diagonal direction
+        let diagDR = this.getDiagDR(y, x);
+        let diagDL = this.getDiagDL(y, x);
 
 
-      //then send through _win to see if any of those are legal sequences of four
-      if (_win(horiz) || _win(vert) || _win(this.diagDR) || _win(this.diagDL)) {
-        if (_win(horiz)) {
-          winningFour = this.getHoriz(y, x);
-        } else if (_win(vert)){
-          winningFour = this.getVert(y, x);
-        } else if (_win(this.diagDR)){
-          winningFour = this.getDiagDR(y, x)
-        } else {
-          winningFour = this.getDiagDL(y, x);
+
+        //then send through _win to see if any of those are legal sequences of four
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          if (_win(horiz)) {
+            console.log("HORIZONTAL WIN")
+            winningFourXYs = this.getHoriz(y, x);
+          } else if (_win(vert)) {
+            console.log("VERTICAL WIN")
+            winningFourXYs = this.getVert(y, x);
+          } else if (_win(diagDR)) {
+            console.log("DIAGONAL DR WIN")
+            winningFourXYs = this.getDiagDR(y, x)
+          } else {
+            console.log("DIAGONAL DL WIN")
+            winningFourXYs = this.getDiagDL(y, x);
+          }
+          //return true if a win
+          return true;
         }
-        //return true if a win
-        return true;
       }
     }
   }
-}
 
 
 
@@ -256,7 +265,7 @@ checkForWin() {
 
 }
 
-let newGame = new Game(1,2,7,8);
+let newGame = new Game(1, 2, 7, 8);
 
 newGame.makeBoard();
 newGame.makeHtmlBoard();
---
+++ connect-four-oo/testing_pythong.py
@@ -0,0 +1,7 @@
+print("testing")
+x = 20
+if x <18:
+    print("too young!")
+else:
+    print("go vote!")
+
